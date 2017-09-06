c28fde54a90027d83b353abcf4238737e378e889
Ian <imatthews009@gmail.com>
2017-09-05 18:44:10 -0700

    added delete, update/edit

--- app/controllers/products_controller.rb
+++ app/controllers/products_controller.rb
@@ -22,4 +22,21 @@ class ProductsController < ApplicationController
         @product = Product.find_by(id: params[:id])
         render "show.html.erb"
     end
+
+    def edit
+        @product = Product.find_by(id: params[:id])
+        render "edit.html.erb"
+    end
+
+    def update
+        @product = Product.find_by(id: params[:id])
+        @product.update({name: params[:name], price: params[:price], image: params[:image], description: params[:description]})
+        @product.save
+        render "update.html.erb"
+    end
+    def destroy
+        @product = Product.find_by(id: params[:id])
+        @product.destroy
+        render "destroy.html.erb"
+    end
 end
